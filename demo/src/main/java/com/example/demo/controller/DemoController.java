98abc90a08b814c2d945dab5de0f947b8e4fa77c
woopaca <jcw001031@gmail.com>
2022-09-15 01:17:50 +0900

    sleep

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -3,24 +3,21 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DemoController {
+
     @GetMapping("hello") //Web Application에서 /hello라고 들어오면 아래 메서드를 호출
     public String hello(Model model){
         model.addAttribute("data", "hello!!");
         return "hello"; //.../resources/templates/hello.html 반환(실행)
     }
 
-    @GetMapping("test")
-    public String test(Model model){
-        model.addAttribute("testData", "Test...");
-        return "test";
-    }
-
-    @GetMapping("demo")
-    public String demo(Model model){
-        model.addAttribute("DData", "Spring boot practice\n");
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
         return "demo";
     }
+
 }

585b84d6277c59bc9580094798617a1442cda436
woopaca <jcw001031@gmail.com>
2022-09-24 23:57:37 +0900

    add sql directory

--- Member-Management/src/main/java/jcw/MemberManagement/controller/MemberController.java
+++ Member-Management/src/main/java/jcw/MemberManagement/controller/MemberController.java
@@ -4,6 +4,7 @@ import jcw.MemberManagement.domain.Member;
 import jcw.MemberManagement.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -35,5 +36,12 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findAllMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
